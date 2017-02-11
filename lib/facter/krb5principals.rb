89c6f81336488c18b0f4d5abddd5da2b9fefde27
Steven Ford <fordste5@msu.edu>
2017-02-10 20:36:48 -0500

    Don't run this fact if klist is not present

--- lib/facter/krb5principals.rb
+++ lib/facter/krb5principals.rb
@@ -2,15 +2,17 @@
 
 Facter.add(:krb5principals) do
   setcode do
-    output = `/usr/bin/klist -k /etc/krb5.keytab`
-    princs = Hash.new
-    
-    output.split(/\n/).each do |line|
-      if ( line =~ /^----|^KVNO|^Keytab/ )
-        next
+  princs = Hash.new
+    if File.file?('/usr/bin/klist')
+      output = `/usr/bin/klist -k /etc/krb5.keytab`
+
+      output.split(/\n/).each do |line|
+        if ( line =~ /^----|^KVNO|^Keytab/ )
+          next
+        end
+        princ = line.gsub(/\s+/m, ' ').strip.split(" ")
+        princs[princ[1]] = princ[0]
       end
-      princ = line.gsub(/\s+/m, ' ').strip.split(" ")
-      princs[princ[1]] = princ[0]
     end
     princs
   end
